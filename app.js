72ffa9877fa0efac4f6fa028fda8805ef4797e3f
Samuel Blake <samuelblake97@gmail.com>
2019-02-07 15:30:02 -0800

    Brief Skeleton
    Two pages, nothing much yet. Most of the necessary files are there though.

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var path = require('path');
 var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
+var userprofile = require('./routes/userprofile');
 // Example route
 // var user = require('./routes/user');
 
@@ -35,6 +36,7 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', index.view);
+app.get('/:username', userprofile.view);
 // Example route
 // app.get('/users', user.list);
 
