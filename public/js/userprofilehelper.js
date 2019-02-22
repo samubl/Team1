4ee38f93d2e082bcfe2cdcedfdf771608f35125d
Samuel Blake <samuelblake97@gmail.com>
2019-02-21 22:49:15 -0800

    Moving Scripts around

--- public/js/userprofilehelper.js
+++ public/js/userprofilehelper.js
@@ -1,8 +1,9 @@
 'use strict';
+var data;
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
-	initializePage();
+    initializePage();
 })
 
 /*
@@ -11,8 +12,29 @@ $(document).ready(function() {
 function initializePage() {
     $('#addsource').click(chooseSource);
     $('#submittedusername').click(addSource);
-
 	// $('#colorBtn').click(randomizeColors);
+}
+
+function addSource(e) {
+    e.preventDefault();
+
+    //accept user inout
+    var username = document.getElementById("Enter username").value;
+    console.log(username);
+
+    var data = $.getJSON('../../data.json', function(json) {
+        console.log(data);
+    });
+
+    if (data.sources.connected == false) {
+        data.sources.connected = true;
+        data.sources.link = "https://open.spotify.com/user/" + username;
+    }
+
+    $('.linked').show();
+    $('.sourceops').hide();
+
+    //TODO update data.json
 }
 
 /*
@@ -36,19 +58,6 @@ function chooseSource(e) {
     //$('.sourceops').html(projectHTML);*/
 	//added get statement
 	//$.get("/project/"+idNumber, callback)
-}
-
-function addSource(e) {
-    e.preventDefault();
-
-    //accept user inout
-    var username = document.getElementById("Enter username").value;
-    console.log(username);
-
-    $('.sourceops').hide();
-
-    $('.mediums').show();
-    //TODO update data.json
 }
 
 function callback(result) {
