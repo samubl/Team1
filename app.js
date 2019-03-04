6908ad8b29a636ca81b4e5f9ab83261b1350199a
Samuel Blake <samuelblake97@gmail.com>
2019-03-04 12:17:53 -0800

    Updated userprofile page, finished two different favorites page
    Added Top 3 Recent favorites songs to user profile page. Added all playlists and 50 favorite artists from different time ranges to favorites page. Other small bug fixes

--- app.js
+++ app.js
@@ -14,11 +14,10 @@ var cookieParser = require('cookie-parser');
 
 var client_id = '4293c4dc48304434aba4777d650d0c6d'; // Your client id
 var client_secret = '91ec585bbb66403f8abbd520784c6d4d'; // Your secret
-var redirect_uri = 'https://a8-team1music.herokuapp.com/callback'; // Your redirect uri
-//var redirect_uri = 'http://localhost:3000/callback';
+//var redirect_uri = 'https://a8-team1music.herokuapp.com/callback'; // Your redirect uri
+var redirect_uri = 'http://localhost:3000/callback';
 
 //route javascript files 
-var index = require('./routes/index');
 var userprofile = require('./routes/userprofile');
 var favorites = require("./routes/favorites")
 var favoritesTemp = require("./routes/favoritesTemp");
